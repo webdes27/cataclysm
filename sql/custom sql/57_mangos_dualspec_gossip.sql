22ae0d08b8b39de7fc575b5d03aad57ca0e330d7
Christian <deathlord22@gmail.com>
2010-04-07 22:04:13 +0200

    try to fix dual spec gossip crash

--- sql/custom sql/57_mangos_dualspec_gossip.sql
+++
@@ -1,5 +0,0 @@
-﻿UPDATE `characters` SET `specCount` = 1, `activeSpec` = 0, `money` = `money`+ 10000000, `at_login` = `at_login` | 4 WHERE `specCount` = 2;
-
-# replace = delete+insert
-REPLACE INTO `gossip_menu_option` VALUES
-(0, 16, 0, 'Purchase a Dual Talent Specialization.', 18, 16, 0, 0, 0, 0, 10000000, 'Are you sure you wish to purchase a Dual Talent Specialization?', 0, 0, 0, 0, 0, 0, 0, 0, 0);
