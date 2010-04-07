2987139c96f669a19ac2be06534cc4a614614061
Christian <deathlord22@gmail.com>
2010-04-07 13:32:18 +0200

    corrected dualspec sql

--- sql/custom sql/57_mangos_dualspec_gossip.sql
+++ sql/custom sql/57_mangos_dualspec_gossip.sql
@@ -1,2 +1,5 @@
-﻿INSERT INTO `gossip_menu_option` VALUES
+﻿UPDATE `characters` SET `specCount` = 1, `activeSpec` = 0, `money` = `money`+ 10000000, `at_login` = `at_login` | 4 WHERE `specCount` = 2;
+
+# replace = delete+insert
+REPLACE INTO `gossip_menu_option` VALUES
 (0, 16, 0, 'Purchase a Dual Talent Specialization.', 18, 16, 0, 0, 0, 0, 10000000, 'Are you sure you wish to purchase a Dual Talent Specialization?', 0, 0, 0, 0, 0, 0, 0, 0, 0);
