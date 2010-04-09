bf912cd6d3a6cd70fb4688e73614ec1596b0d126
kicho <deathlord22@gmail.com>
2010-04-09 19:43:41 +0200

    this should restore compile

--- src/game/Player.h
+++ src/game/Player.h
@@ -1440,11 +1440,6 @@ class MANGOS_DLL_SPEC Player : public Unit
 
         void Initialize(uint32 guid);                      // AHBot
 
-
-        static uint32 GetZoneIdFromDB(uint64 guid);
-        static uint32 GetLevelFromDB(uint64 guid);
-        static bool   LoadPositionFromDB(uint32& mapid, float& x,float& y,float& z,float& o, bool& in_flight, uint64 guid);*/
-
 		static uint32 GetZoneIdFromDB(uint64 guid);
         static uint32 GetLevelFromDB(uint64 guid);
         static bool   LoadPositionFromDB(uint32& mapid, float& x,float& y,float& z,float& o, bool& in_flight, uint64 guid);
