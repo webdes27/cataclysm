7f6bc08ea581d44cc3591708f0175619d6800f52
Kicho <deathlord22@gmail.com>
2010-06-02 20:01:01 +0200

    dbc file structure

--- src/game/DBCEnums.h
+++ src/game/DBCEnums.h
@@ -22,7 +22,7 @@
 // Client expected level limitation, like as used in DBC item max levels for "until max player level"
 // use as default max player level, must be fit max level for used client
 // also see MAX_LEVEL and STRONG_MAX_LEVEL define
-#define DEFAULT_MAX_LEVEL 90
+#define DEFAULT_MAX_LEVEL 85
 
 // client supported max level for player/pets/etc. Avoid overflow or client stability affected.
 // also see GT_MAX_LEVEL define
@@ -242,8 +242,8 @@ enum AreaFlags
     AREA_FLAG_UNK7                  = 0x00400000,           // Warsong Hold, Acherus: The Ebon Hold, New Agamand Inn, Vengeance Landing Inn
     AREA_FLAG_UNK8                  = 0x00800000,           // Westguard Inn, Acherus: The Ebon Hold, Valgarde
     AREA_FLAG_OUTDOOR_PVP           = 0x01000000,           // Wintergrasp and it's subzones
-    AREA_FLAG_UNK9                  = 0x02000000,           // unknown
-    AREA_FLAG_UNK10                 = 0x04000000,           // unknown
+    AREA_FLAG_INSIDE                = 0x02000000,           // used for determinating spell related inside/outside questions in Map::IsOutdoors
+    AREA_FLAG_OUTSIDE               = 0x04000000,           // used for determinating spell related inside/outside questions in Map::IsOutdoors
     AREA_FLAG_CAN_HEARTH_AND_RES    = 0x08000000            // Wintergrasp and it's subzones
     // 0x20000000 not flyable?
 };
@@ -407,5 +407,6 @@ enum SpellEffectIndex
 };
 
 #define MAX_EFFECT_INDEX 3
+#define MAX_REAGENTS 8
 
 #endif
